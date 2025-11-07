5e14e92b4d6d92d5913c9595d9acfb13236a36aa
M Agung Dirgantara Azly Bagus Wibawa <agungmasda29@gmail.com>
2025-11-07 01:00:52 +0000

    feat(websocket): initial implementation of websocket integration

--- public/global.js
+++ public/global.js
@@ -135,3 +135,11 @@ jQuery(() => {
     window.location.href = $(this).attr('href');
   });
 });
+
+const socket = io({
+  path: '/ws'
+});
+
+socket.on('connect', () => {
+  console.log('websocket connected');
+});
